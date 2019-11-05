9e7250c16d8298649651763b4d97add93d5120df
wangzb <wangzb@ti-net.com>
2019-11-05 16:30:11 +0800

    升级2.0.9，修复咨询取消异常的问题

--- clink-serversdk/src/main/java/com/tinet/clink/openapi/request/call/manage/UnpauseClientRequest.java
+++ clink-serversdk/src/main/java/com/tinet/clink/openapi/request/call/manage/UnpauseClientRequest.java
@@ -51,6 +51,6 @@ public class UnpauseClientRequest extends AbstractRequestModel<UnpauseClientResp
 
     @Override
     public Class<UnpauseClientResponse> getResponseClass() {
-        return null;
+        return UnpauseClientResponse.class;
     }
 }
