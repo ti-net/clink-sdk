65d9bcd4f763d9547be50a3cd40034aec771b295
Tinet-liuhy <liuhy@ti-net.com.cn>
2020-07-30 16:35:06 +0800

    添加业务记录接口

--- clink-serversdk/src/main/java/com/tinet/clink/openapi/request/business/ListBusinessRequest.java
+++ clink-serversdk/src/main/java/com/tinet/clink/openapi/request/business/ListBusinessRequest.java
@@ -1,10 +1,12 @@
 package com.tinet.clink.openapi.request.business;
 
 import com.tinet.clink.openapi.PathEnum;
-import com.tinet.clink.openapi.model.BusinessSearchModel;
 import com.tinet.clink.openapi.request.AbstractRequestModel;
 import com.tinet.clink.openapi.response.Business.ListBusinessResponse;
 import com.tinet.clink.openapi.utils.HttpMethodType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**业务记录查询
  *
@@ -14,14 +16,77 @@ import com.tinet.clink.openapi.utils.HttpMethodType;
 public class ListBusinessRequest extends AbstractRequestModel<ListBusinessResponse> {
 
 
-    private BusinessSearchModel businessSearchModel;
+    private Long startTime;
 
-    public BusinessSearchModel getBusinessSearchModel() {
-        return businessSearchModel;
+    private Long endTime;
+
+    private Map<String, String> customize = new HashMap<String, String>();
+
+    /**
+     * 偏移量，范围 0-10000，默认值为 0
+     */
+    private Integer offset;
+
+    /**
+     * 查询条数，范围 10-100，默认值为 10
+     */
+    private Integer limit;
+
+    public Integer getOffset() {
+        return offset;
     }
 
-    public void setBusinessSearchModel(BusinessSearchModel businessSearchModel) {
-        this.businessSearchModel = businessSearchModel;
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+        if (offset != null) {
+            putBodyParameter("offset", offset);
+        }
+    }
+
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+        if (limit != null) {
+            putQueryParameter("limit", limit);
+        }
+    }
+
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+        if (startTime != null) {
+            putQueryParameter("startTime", startTime);
+        }
+    }
+
+    public Long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Long endTime) {
+        this.endTime = endTime;
+        if (endTime != null) {
+            putQueryParameter("endTime", endTime);
+        }
+    }
+
+    public Map<String, String> getCustomize() {
+        return customize;
+    }
+
+    public void setCustomize(Map<String, String> customize) {
+        this.customize = customize;
+        if (customize != null) {
+            putQueryParameter("customize", customize);
+        }
     }
 
     public ListBusinessRequest() {
@@ -32,4 +97,6 @@ public class ListBusinessRequest extends AbstractRequestModel<ListBusinessRespon
     public Class<ListBusinessResponse> getResponseClass() {
         return ListBusinessResponse.class;
     }
+
+
 }
