ecfa55ff087907a2e9f1dee8e62366499f8df005
Tinet-huwk <huwk@ti-net.com.cn>
2020-11-18 09:59:24 +0800

    comment submit

--- clink-serversdk/src/main/java/com/tinet/clink/openapi/request/ticket/TicketUploadRequest.java
+++ clink-serversdk/src/main/java/com/tinet/clink/openapi/request/ticket/TicketUploadRequest.java
@@ -4,11 +4,11 @@ import com.tinet.clink.openapi.request.AbstractRequestModel;
 import com.tinet.clink.openapi.response.ticket.TicketUploadResponse;
 import com.tinet.clink.openapi.utils.HttpMethodType;
 
-import java.io.File;
-import java.util.List;
-import java.util.Map;
-
 /**
+ * 通用上传文件请求
+ * 1、重写isMultipartFormData方法，使其支持 multipart form data 请求方式
+ * 2、如果有text参数，传入到父类的到 model中；如果有附件，传入到父类的 fileMap中。
+ *
  * @author huwk
  * @date 2020/11/15
  **/
@@ -16,15 +16,6 @@ public class TicketUploadRequest extends AbstractRequestModel<TicketUploadRespon
 
     public TicketUploadRequest() {
         super("/upload", HttpMethodType.POST);
-    }
-
-    public void setModel(String model) {
-
-        this.model = model;
-    }
-
-    public void setFileMap(Map<String,List<File>> fileMap) {
-        this.fileMap = fileMap;
     }
 
     @Override
