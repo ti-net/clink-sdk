46b9bb7d8b4d83280efb20d49f7fbe200b9c050a
wangll <wangll@ti-net.com.cn>
2021-07-29 11:11:28 +0800

    提交代码

---
+++ clink-serversdk/src/main/java/com/tinet/clink/openapi/request/stat/StatInvestigationByCnoRequest.java
@@ -0,0 +1,40 @@
+package com.tinet.clink.openapi.request.stat;
+
+import com.tinet.clink.openapi.PathEnum;
+import com.tinet.clink.openapi.response.stat.StatInvestigationByCnoResponse;
+import com.tinet.clink.openapi.utils.HttpMethodType;
+
+/**
+ * @author yuanfeng@ti-net.com.cn
+ * @date 2021/7/20 16:51
+ */
+public class StatInvestigationByCnoRequest extends AbstractStatRequest<StatInvestigationByCnoResponse> {
+
+  /**
+   * 呼叫类型 呼入: ib; 呼出: ob
+   */
+  private String callType;
+
+  public String getCallType() {
+    return callType;
+  }
+
+  public void setCallType(String callType) {
+    this.callType = callType;
+    if (callType != null && !"ib".equals(callType) && !"ob".equals(callType)) {
+      throw new IllegalArgumentException("callType must be ib or ob !");
+    }
+    if (callType != null) {
+      putQueryParameter("callType", callType);
+    }
+  }
+
+  @Override
+  public Class<StatInvestigationByCnoResponse> getResponseClass() {
+    return StatInvestigationByCnoResponse.class;
+  }
+
+  public StatInvestigationByCnoRequest() {
+    super(PathEnum.StatInvestigationByCno.value(), HttpMethodType.POST);
+  }
+}
