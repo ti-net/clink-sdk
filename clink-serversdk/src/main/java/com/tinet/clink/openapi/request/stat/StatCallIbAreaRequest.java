e01cd2fec8132d61ceb8c580e64a7bd49a4102a3
Tinet-wangli <wangli@ti-net.com.cn>
2022-05-25 16:50:15 +0800

    feat:@QBYC-371_新增呼入报表—按地区统计

---
+++ clink-serversdk/src/main/java/com/tinet/clink/openapi/request/stat/StatCallIbAreaRequest.java
@@ -0,0 +1,20 @@
+package com.tinet.clink.openapi.request.stat;
+
+import com.tinet.clink.openapi.PathEnum;
+import com.tinet.clink.openapi.response.stat.StatHotlineIbResponse;
+import com.tinet.clink.openapi.response.stat.StatQueueResponse;
+import com.tinet.clink.openapi.utils.HttpMethodType;
+
+
+public class StatCallIbAreaRequest extends AbstractStatRequest<StatCallIbAreaResponse> {
+
+    @Override
+    public Class<StatCallIbAreaResponse> getResponseClass() {
+        return StatCallIbAreaResponse.class;
+    }
+
+    public StatCallIbAreaRequest() {
+        super(PathEnum.StatCallIbArea.value(), HttpMethodType.POST);
+    }
+
+}
