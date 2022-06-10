98ea1fcb2bba6f6dab0b5dbf219654fc71c29f69
midong <midong@ti-net.com.cn>
2022-06-10 14:23:10 +0800

    fix：恢复他原来的

--- clink-serversdk/src/test/java/com/tinet/clink/openapi/chat/ChatRobotTransferTest.java
+++ clink-serversdk/src/test/java/com/tinet/clink/openapi/chat/ChatRobotTransferTest.java
@@ -3,7 +3,9 @@ package com.tinet.clink.openapi.chat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tinet.clink.openapi.Client;
 import com.tinet.clink.openapi.ClientConfiguration;
+import com.tinet.clink.openapi.request.chat.ChatQuitQueueRequest;
 import com.tinet.clink.openapi.request.chat.ChatRobotTransferRequest;
+import com.tinet.clink.openapi.response.chat.ChatQuitQueueResponse;
 import com.tinet.clink.openapi.response.chat.ChatRobotTransferResponse;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +20,7 @@ public class ChatRobotTransferTest {
     @Before
     public void init() {
         System.out.println("----------------------------->");
-        configuration = new ClientConfiguration("d8a0213a66aa9ff4fab60aa5ea99a40e", "IO9Fpa392A3y54375Tvu");
+        configuration = new ClientConfiguration("706ff5f9bbb10286dcf7545262a7d702", "IO9Fpa392A3y54375Tvu");
         configuration.setScheme("http");
         configuration.setHost("api-bj-test3.clink.cn");
 
