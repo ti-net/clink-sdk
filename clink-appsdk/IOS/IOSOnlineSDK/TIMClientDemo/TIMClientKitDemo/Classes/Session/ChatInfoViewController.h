019cc8fd312a91969d772a8cdeb3bd908d8b3f40
gyb1314 <gyb_1314@126.com>
2022-06-27 22:47:03 +0800

    更新SDK v1.3.5以及Demo

--- clink-appsdk/IOS/IOSOnlineSDK/TIMClientDemo/TIMClientKitDemo/Classes/Session/ChatInfoViewController.h
+++ clink-appsdk/IOS/IOSOnlineSDK/TIMClientDemo/TIMClientKitDemo/Classes/Session/ChatInfoViewController.h
@@ -8,7 +8,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ChatInfoViewController : TOSCustomerChatVC//TIMChatViewController
+@interface ChatInfoViewController : TOSCustomerChatVC
 
 @end
 
--- clink-appsdk/IOS/IOSOnlineSDK/TIMClientDemo/TIMClientKitDemo/Classes/Session/kefuVC.m
+++ clink-appsdk/IOS/IOSOnlineSDK/TIMClientDemo/TIMClientKitDemo/Classes/Session/kefuVC.m
@@ -74,23 +74,12 @@
 
 
 -(void)kefuAction{
-    /*访客初始化回调
-     当APP端IM mqtt 连接就绪，主动通知服务端，APP端调用该接口后服务端
-     就开始会话流程逻辑创建会话*/
-//    [[OnlineRequestManager sharedCustomerManager] visitorReadyWithDict:@{} success:^(TOSSessionInfoModel * _Nonnull sessModel) {
-//
-//    } error:^(TIMConnectErrorCode errCode, NSString * _Nonnull errorDes) {
-//
-//    }];
-
-    
-    //创建会话成功，进入聊天页面
+    //进入聊天页面
     ChatInfoViewController *chatVC = [[ChatInfoViewController alloc] init];
     chatVC.titleName = @"客服";
     chatVC.appName = @"客服SDK";
     self.hidesBottomBarWhenPushed  = YES;
     [self.navigationController pushViewController:chatVC animated:YES];
-    
 }
 
 
