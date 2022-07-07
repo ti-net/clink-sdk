31d38a6bd8504f29e24511c74673ea97816c5a54
Tinet-yinzk <yinzk@ti-net.com.cn>
2022-07-07 17:53:20 +0800

    doc: QBYC-947@【售后-VIP】【啄木鸟】优化队列监控接口 监控-队列状态监控-增加监控范围参数

--- clink-serversdk/src/main/java/com/tinet/clink/openapi/request/monitor/QueueStatusRequest.java
+++ clink-serversdk/src/main/java/com/tinet/clink/openapi/request/monitor/QueueStatusRequest.java
@@ -1,8 +1,8 @@
 package com.tinet.clink.openapi.request.monitor;
 
 import com.tinet.clink.openapi.PathEnum;
+import com.tinet.clink.openapi.constant.QueueStatusScope;
 import com.tinet.clink.openapi.request.AbstractRequestModel;
-import com.tinet.clink.openapi.response.monitor.AgentStatusResponse;
 import com.tinet.clink.openapi.response.monitor.QueueStatusResponse;
 import com.tinet.clink.openapi.utils.HttpMethodType;
 
@@ -18,6 +18,12 @@ public class QueueStatusRequest extends AbstractRequestModel<QueueStatusResponse
      * 队列号数组
      */
     private String[] qnos;
+
+    /**
+     * 返回值范围
+     * all:全部返回，queueParams：队列参数；agentStatus：队列成员；queueEntries:排队成员
+     */
+    private String statusScope = QueueStatusScope.STATUS_SCOPE_ALL;
 
     public QueueStatusRequest() {
         super(PathEnum.QueueStatus.value(), HttpMethodType.GET);
@@ -38,6 +44,17 @@ public class QueueStatusRequest extends AbstractRequestModel<QueueStatusResponse
             for (int i = 0; i < qnos.length; i++) {
                 putQueryParameter("qnos[" + i + "]", qnos[i]);
             }
+        }
+    }
+
+    public String getStatusScope() {
+        return statusScope;
+    }
+
+    public void setStatusScope(String statusScope) {
+        this.statusScope = statusScope;
+        if (statusScope != null) {
+            putQueryParameter("statusScope", statusScope);
         }
     }
 }
