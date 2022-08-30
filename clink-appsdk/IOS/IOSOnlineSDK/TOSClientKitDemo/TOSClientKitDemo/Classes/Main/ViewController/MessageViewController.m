beaaccd2bf35993cfd4579d1356d08dea0e2e9d6
18734791527 <1142445395@qq.com>
2022-08-31 01:44:24 +0800

    升级SDK

--- clink-appsdk/IOS/IOSOnlineSDK/TOSClientKitDemo/TOSClientKitDemo/Classes/Main/ViewController/MessageViewController.m
+++ clink-appsdk/IOS/IOSOnlineSDK/TOSClientKitDemo/TOSClientKitDemo/Classes/Main/ViewController/MessageViewController.m
@@ -7,6 +7,8 @@
 
 #import "MessageViewController.h"
 
+#import "BaiduMapViewController.h"
+
 @interface MessageViewController ()
 
 @end
@@ -21,6 +23,15 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:animated];
+}
+
+/// 扩展面板，自定义按钮事件 （需要在子类实现这个方法）
+- (void)didClinkCustomExtendBoardItemAction:(TOSKitExtendBoardItemModel *)item {
+    
+    if (item.type == TOSChatBoxExtendBoardTypeCustom &&
+        item.index == 10006) {
+        [self.navigationController pushViewController:[[BaiduMapViewController alloc] init] animated:YES];
+    }
 }
 
 @end
