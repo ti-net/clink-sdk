36764f48f89be0e07358f02a00686bc07bc7cc37
liyj <liyj@ti-net.com.cn>
2023-06-29 10:16:10 +0800

    fix:QBYC-7120@【零跑汽车】增加短信模板接口 | 修改sdk

--- clink-serversdk/src/main/java/com/tinet/clink/cc/model/SmsTemplateModel.java
+++ clink-serversdk/src/main/java/com/tinet/clink/cc/model/SmsTemplateModel.java
@@ -11,19 +11,19 @@ import java.util.Date;
 public class SmsTemplateModel {
 
     /**
-     * 模板id
-     */
-    private Integer id;
-
-    /**
      * 模板名称
      */
     private String name;
 
     /**
-     * 内容
+     * 模板内容
      */
     private String content;
+
+    /**
+     * 模板id
+     */
+    private String copTemplateId;
 
     /**
      * 审核状态
@@ -36,81 +36,67 @@ public class SmsTemplateModel {
     private String comment;
 
     /**
-     * 创建时间
+     * 创建时间，在前端展示叫提交时间
      */
     private Date createTime;
 
-    /**
-     * 更新时间
-     */
-    private Date updateTime;
-
-    public Integer getId() {
-        return id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getContent() {
+        return content;
+    }
+
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getCopTemplateId() {
+        return copTemplateId;
+    }
+
+    public void setCopTemplateId(String copTemplateId) {
+        this.copTemplateId = copTemplateId;
+    }
+
+    public Integer getStatus() {
+        return status;
     }
 
     public void setStatus(Integer status) {
         this.status = status;
     }
 
+    public String getComment() {
+        return comment;
+    }
+
     public void setComment(String comment) {
         this.comment = comment;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
     }
 
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
 
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
     @Override
     public String toString() {
         return "SmsTemplateModel{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
                 ", content='" + content + '\'' +
+                ", copTemplateId='" + copTemplateId + '\'' +
                 ", status=" + status +
                 ", comment='" + comment + '\'' +
                 ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
                 '}';
     }
 }
