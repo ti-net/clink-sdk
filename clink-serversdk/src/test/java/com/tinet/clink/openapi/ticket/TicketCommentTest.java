12239b9f8b4b902cd4517b3c437a2244de878c6e
linyang <linyang@ti-net.com.cn>
2024-07-03 15:46:15 +0800

    备注接口增加参数

--- clink-serversdk/src/test/java/com/tinet/clink/openapi/ticket/TicketCommentTest.java
+++ clink-serversdk/src/test/java/com/tinet/clink/openapi/ticket/TicketCommentTest.java
@@ -21,9 +21,11 @@ public class TicketCommentTest extends AbstractTest {
     public void ticketComment(){
         // 创建访问服务的client实例并初始化
         ClientConfiguration configuration = new ClientConfiguration(
-                "AK",          // AccessKeyId
-                "SK");     // AccessKeySecret
-        configuration.setHost("host");
+                "babc37aa470a92102016186b1cb79a82",          // AccessKeyId
+                "vtq672M5Y48aI3728m14");     // AccessKeySecret
+        configuration.setHost("127.0.0.1");
+        configuration.setPort(8111);
+        configuration.setScheme("http");
 
         Client client = new Client(configuration);
 
@@ -31,12 +33,16 @@ public class TicketCommentTest extends AbstractTest {
         TicketCommentRequest ticketCommentRequest = new TicketCommentRequest();
         // 请求参数
         TicketCommentModel ticketCommentModel = new TicketCommentModel();
-        ticketCommentModel.setId(1);
+        ticketCommentModel.setId(1773804);
         ticketCommentModel.setContent("content22222");
+        ticketCommentModel.setCommentatorId("linyang3");
+        ticketCommentModel.setCommentatorIdType(4);
+        ticketCommentModel.setStateId("827");
+        ticketCommentModel.setStateIdType(1);
 
         // 附件信息
         List<File> fileList = new ArrayList<>();
-        File file = new File("51915.xlsx");
+        File file = new File("D:\\钉钉三方应用二维码.png");
         fileList.add(file);
         Map<String, List<File>> fileMap = new HashMap<>();
         fileMap.put("fileName", fileList);
