043dd16526349f154fe7e05bb85f26c6edbb9db6
tinet-yangcr <yangcr@ti-net.com.cn>
2024-07-30 14:34:11 +0800

    feature:增加企微群客户查询与修改

--- clink-serversdk/src/main/java/com/tinet/clink/crm/PathEnum.java
+++ clink-serversdk/src/main/java/com/tinet/clink/crm/PathEnum.java
@@ -5,10 +5,15 @@ package com.tinet.clink.crm;
  * @date 2019/2/19
  */
 public enum PathEnum {
+    /**
+     * 客户资料
+     */
     //客户资料-获取可用查询参数
     CustomerParam("crm/customer_params"),
+
     //客户资料-查询客户资料列表
     ListCustomers("crm/list_customers"),
+
     //创建客户资料
     CreateCustomer("crm/create_customer"),
 
@@ -17,17 +22,22 @@ public enum PathEnum {
 
     //更新客户资料
     UpdateCustomerByExternalId("crm/update_customer_by_external_id"),
+
     //创建客户资料
     ListCustomerField("crm/list_customer_field"),
+
     // 删除客户资料
     DeleteCustomer("crm/delete_customer"),
+
     //查询客户资料
     QueryCustomer("crm/query_customer"),
 
     //更新客户资料关联
     UpdateCustomerRelevancy("crm/update_customer_relevancy"),
 
-
+    /**
+     * 业务记录
+     */
     //查询业务记录的自定义字段
     ListBusinessField("crm/list_business_customize_field"),
 
@@ -44,10 +54,20 @@ public enum PathEnum {
     CreateBusiness("crm/create_business"),
 
     //查询业务记录自定义字段
-    ListBusinessFieldInfo("crm/list_business_field");
+    ListBusinessFieldInfo("crm/list_business_field"),
 
     /**
-     * 外呼任务创建
+     * 企微群客户
+     */
+    //查询企微群客户
+    QueryGroupCustomer("crm/query_group_customer"),
+
+    //修改企微群客户
+    UpdateGroupCustomer("crm/update_group_customer"),
+    ;
+
+    /**
+     * 请求路径
      */
     private String value;
 
